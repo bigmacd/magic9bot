de77333fb88a1e95ae557427c269b14c3ba2196d
ETS\martin.cooley <mcooley@emoney.com>
2017-02-02 16:24:17 -0500

    changed for botframeworkbot

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 module.exports = {
     token: "token-here"
 };
+var appid = "d7b808d0-9a66-427e-88c8-36272527d53c"
+var p = "SS9f39QXAmcC9uXkHGcmK0P"
--- index.js
+++ index.js
@@ -3,51 +3,54 @@ var answer = require('./messages');
 var wolfram = require('wolfram-alpha').createClient("V66LW7-2447QJTY66", null);
 
 var teammembers = [
-   "Proctor",
-   "Weixin",
-   "Martin",
-   "Nick",
-   "Giles",
-   "Thang"];
+    "Trey",
+    "Sam",
+    "Martin",
+    "Dana",
+    "Everett",
+    "Ryan",
+    "Erika",
+    "Zak",
+    "Gloria"
+];
 
-var controller = Botkit.slackbot();
+//var controller = Botkit.slackbot(); // slack
+var controller = Botkit.botframeworkbot({});
 var wolfMessage;
 
 function wolfCallback(response) {
     reply(wolfMessage, response);
 }
 
-var getData = function (err, result) {
-  if (!err) {
-      var response = '';
-      var l = result.length;
-      if (l == 0) { response = "Sorry, I don't know."}
-      else {
-          for (var i = 0; i < l; i++) {
-              j = result[i];
-              var subpods = j['subpods'];
-              ll = subpods.length;
-              for (ii = 0; ii < ll; ii++) {
-                  sp = subpods[ii];
-                  response += sp['text'];
-                  response += ' ';
-              }
-          }
-      }
-      wolfCallback(response);
-  }
-  else {
-      wolfCallback(err);
-  }
+var getData = function(err, result) {
+    if (!err) {
+        var response = '';
+        var l = result.length;
+        if (l == 0) { response = "Sorry, I don't know." } else {
+            for (var i = 0; i < l; i++) {
+                j = result[i];
+                var subpods = j['subpods'];
+                ll = subpods.length;
+                for (ii = 0; ii < ll; ii++) {
+                    sp = subpods[ii];
+                    response += sp['text'];
+                    response += ' ';
+                }
+            }
+        }
+        wolfCallback(response);
+    } else {
+        wolfCallback(err);
+    }
 };
 
 //answer.loadFirstMessages('localhost', '3000', '/1');
 //answer.loadSecondMessages('localhost', '3000', '/2');
 
-controller.hears('',["direct_mention", "mention", "direct_message"],function(bot,message) {
-  // do something to respond to message
-  // all of the fields available in a normal Slack message object are available
-  // https://api.slack.com/events/message
+controller.hears('', ["direct_mention", "mention", "direct_message"], function(bot, message) {
+    // do something to respond to message
+    // all of the fields available in a normal Slack message object are available
+    // https://api.slack.com/events/message
 
     var response = answer.getRandomFirstLine();
     var wasSaid = message['text'];
@@ -55,16 +58,13 @@ controller.hears('',["direct_mention", "mention", "direct_message"],function(bot
     if (/\b[p|P]oints\b/.test(wasSaid)) {
         response += Math.floor((Math.random() * 20) + 1) + " points";
         reply(message, response);
-    }
-    else if (/\b[w|W]ho\b/.test(wasSaid)) {
+    } else if (/\b[w|W]ho\b/.test(wasSaid)) {
         response += teammembers[Math.floor((Math.random() * teammembers.length))];
         reply(message, response);
-    }
-    else if (/\b^[w|W]hat\b/.test(wasSaid)) {
+    } else if (/\b^[w|W]hat\b/.test(wasSaid)) {
         wolfMessage = message;
         wolfram.query(wasSaid, getData);
-    }
-    else {
+    } else {
         response += answer.getRandomSecondLine();
         reply(message, response);
     }
@@ -72,20 +72,19 @@ controller.hears('',["direct_mention", "mention", "direct_message"],function(bot
 
 
 var bot = controller.spawn({
-  //token: require('./config').token
-  token: process.env.token
+    //token: process.env.token  // slack
+    appId: process.env.app_id,
+    appPassword: process.env.app_password
 });
 
 
-bot.startRTM(function(err,bot,payload) {
-  if (err) {
-    throw new Error('Could not connect to Slack');
-  }
+bot.startRTM(function(err, bot, payload) {
+    if (err) {
+        throw new Error('Could not connect to Slack');
+    }
 });
 
 
 function reply(message, answer) {
-  bot.reply(message, answer);
+    bot.reply(message, answer);
 }
-
-
